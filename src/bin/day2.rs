de00699f9272a785073aa707077d51e12456a986
Girts Folkmanis <girtsf@users.noreply.github.com>
2019-12-22 15:42:15 -0800

    day5: part 1

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -1,3 +1,5 @@
+// Day 2 and day 5 runner.
+
 #![allow(dead_code)]
 
 extern crate adv_2019;
@@ -9,5 +11,9 @@ fn main() {
     // intcode::day2_part2();
     let filename = args().nth(1).expect("no filename given");
     let mut state = State::from_file(&filename);
+    for arg in args().skip(2) {
+        state.add_input(arg.parse::<i32>().unwrap());
+    }
     state.run(true);
+    dbg!(state.outputs());
 }
--- src/intcode.rs
+++ src/intcode.rs
@@ -1,11 +1,15 @@
+use std::collections::VecDeque;
 /// Intcode interpreter.
 use std::fs;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Default, Clone)]
 pub struct State {
-    memory: Vec<usize>,
+    memory: Vec<i32>,
     /// Instruction pointer.
-    ip: usize,
+    ip: i32,
+
+    inputs: VecDeque<i32>,
+    outputs: Vec<i32>,
 }
 
 impl State {
@@ -16,17 +20,15 @@ impl State {
 
     /// Creates State by parsing a comma delimited string of integers.
     pub fn from_string(s: &str) -> State {
-        let v: Vec<usize> = s
+        let v: Vec<i32> = s
             .trim()
             .split(',')
-            .map(|x| x.parse::<usize>().unwrap())
+            .map(|x| x.parse::<i32>().unwrap())
             .collect();
-        State { memory: v, ip: 0 }
-    }
-
-    /// Returns value at address 0.
-    pub fn get_output(&self) -> usize {
-        self.memory[0]
+        State {
+            memory: v,
+            ..Default::default()
+        }
     }
 
     /// Returns memory as comma-delimited string.
@@ -37,43 +39,132 @@ impl State {
         )
     }
 
+    fn num_to_addr_usize(&self, addr: i32) -> usize {
+        if addr < 0 {
+            panic!("address {} is below zero", addr);
+        } else if addr >= self.memory.len() as i32 {
+            panic!(
+                "address {} is higher than memory size {}",
+                addr,
+                self.memory.len()
+            );
+        }
+        addr as usize
+    }
+
+    /// Reads value at given address.
+    fn read(&self, addr: i32) -> i32 {
+        self.memory[self.num_to_addr_usize(addr)]
+    }
+
     /// Reads value at given address, then reads value that address points at.
-    fn read_indirect(&self, addr: usize) -> usize {
-        self.memory[self.memory[addr]]
+    fn read_indirect(&self, addr: i32) -> i32 {
+        let read_addr = self.read(addr);
+        self.memory[self.num_to_addr_usize(read_addr)]
+    }
+
+    /// Writes value to given address.
+    fn write(&mut self, addr: i32, value: i32) {
+        let write_addr_usize = self.num_to_addr_usize(addr);
+        self.memory[write_addr_usize] = value;
     }
 
     /// Writes value to address from given cell.
-    fn write_indirect(&mut self, addr: usize, value: usize) {
-        let write_addr = self.memory[addr];
-        self.memory[write_addr] = value;
+    fn write_indirect(&mut self, addr: i32, value: i32) {
+        let write_addr = self.read(addr);
+        let write_addr_usize = self.num_to_addr_usize(write_addr);
+        self.memory[write_addr_usize] = value;
+    }
+
+    /// Reads immediate or position, based on mode.
+    fn read_with_mode(&self, mode: i32, value: i32) -> i32 {
+        match mode {
+            0 => self.read(value),
+            1 => value,
+            _ => panic!("invalid mode: {}", mode),
+        }
+    }
+
+    /// Returns first argument after instruction pointer.
+    fn arg1(&self) -> i32 {
+        self.read(self.ip + 1)
+    }
+
+    /// Returns first argument after instruction pointer.
+    fn arg2(&self) -> i32 {
+        self.read(self.ip + 2)
+    }
+
+    /// Returns first argument after instruction pointer.
+    fn arg3(&self) -> i32 {
+        self.read(self.ip + 3)
     }
 
     /// Steps the intcode computer by one step.
     ///
     /// Returns true iff we should keep executing.
     fn step(&mut self) -> bool {
-        let opcode = self.memory[self.ip];
-        // println!("opcode: {}", opcode);
-        let result = match opcode {
-            1 | 2 => {
-                // println!("add");
-                let in1 = self.read_indirect(self.ip + 1);
-                let in2 = self.read_indirect(self.ip + 2);
-                let write_addr = self.ip + 3;
-                if opcode == 1 {
-                    self.write_indirect(write_addr, in1 + in2);
+        let opcode = self.read(self.ip);
+        assert!(opcode >= 0);
+
+        let instruction = opcode % 100;
+        let mode1 = (opcode / 100) % 10;
+        let mode2 = (opcode / 1000) % 10;
+        let mode3 = (opcode / 10000) % 10;
+        match instruction {
+            // Add or multiply:
+            01 | 02 => {
+                let lhs = self.read_with_mode(mode1, self.arg1());
+                let rhs = self.read_with_mode(mode2, self.arg2());
+                // Writes are always in position mode.
+                assert!(mode3 == 0);
+                let write_addr = self.arg3();
+                let result = if instruction == 01 {
+                    lhs + rhs
                 } else {
-                    self.write_indirect(write_addr, in1 * in2);
-                }
-                true
+                    lhs * rhs
+                };
+                self.write(write_addr, result);
+                self.ip += 4;
             }
-            99 => false,
+            // Input:
+            03 => {
+                // Writes are always in position mode.
+                assert!(mode1 == 0);
+                assert!(!self.inputs.is_empty());
+                let write_addr = self.arg1();
+                let value = self.inputs.pop_front().unwrap();
+                println!("INPUT: writing {} to {}", value, write_addr);
+                self.write(write_addr, value);
+                self.ip += 2;
+            }
+            // Output:
+            04 => {
+                let value = self.read_with_mode(mode1, self.arg1());
+                println!("OUTPUT: {}", value);
+                self.outputs.push(value);
+                self.ip += 2;
+            }
+            99 => {
+                self.ip += 1;
+                if !self.inputs.is_empty() {
+                    panic!("not all inputs were used!");
+                }
+                return false;
+            }
             _ => {
-                panic!("unknown opcode: {}", opcode);
+                panic!("unknown instruction/opcode: {}", opcode);
             }
         };
-        self.ip += 4;
-        result
+        true
+    }
+
+    pub fn add_input(&mut self, val: i32) {
+        self.inputs.push_back(val);
+    }
+
+    pub fn outputs(&self) -> &Vec<i32> {
+        &self.outputs
     }
 
     /// Runs until program finishes.
@@ -94,7 +185,7 @@ impl State {
 
 /// "Find the input noun and verb that cause the program to produce the output 19690720. What is
 /// 100 * noun + verb?"
-pub fn day2_part2() -> usize {
+pub fn day2_part2() -> i32 {
     let mut state = State::from_file("inputs/day2_input.txt");
     for noun in 0..=99 {
         state.memory[1] = noun;
@@ -102,7 +193,7 @@ pub fn day2_part2() -> usize {
             state.memory[2] = verb;
             let mut state2 = state.clone();
             state2.run(false);
-            if state2.get_output() == 19690720 {
+            if state2.memory[0] == 19690720 {
                 return 100 * noun + verb;
             }
         }
@@ -133,5 +224,28 @@ mod tests {
     #[test]
     fn test_day2_part2() {
         assert_eq!(day2_part2(), 9425);
+    }
+
+    #[test]
+    fn test_day3_example() {
+        let mut state = State::from_string("1002,4,3,4,33");
+        state.run(true);
+        assert_eq!(state.memory_to_string(), "1002,4,3,4,99");
+    }
+
+    #[test]
+    fn test_day3_input() {
+        let mut state = State::from_string("3,0,99");
+        state.add_input(-42);
+        state.run(true);
+        assert_eq!(state.memory_to_string(), "-42,0,99");
+    }
+
+    #[test]
+    fn test_day3_output() {
+        let mut state = State::from_string("104,42,4,0,99");
+        state.run(true);
+        assert_eq!(state.memory_to_string(), "104,42,4,0,99");
+        assert_eq!(state.outputs, vec![42, 104]);
     }
 }
