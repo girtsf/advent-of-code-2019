0dcfe1eb088995b9f18de4f00a0c72fdfbc44ccd
Girts Folkmanis <opensource@girts.me>
2020-01-25 14:23:49 -0800

    day8: part 2

--- src/bin/day8.rs
+++ src/bin/day8.rs
@@ -2,34 +2,41 @@ use std::env::args;
 use std::fs;
 
 struct Layer {
-    pixels: Vec<Vec<char>>,
+    pixels: Vec<char>,
 }
 
 impl Layer {
-    fn parse(pixels: &[char], width: usize) -> Self {
-        // XXX
-        Layer {
-            pixels: pixels
-                .chunks(width)
-                .map(|x| x.iter().cloned().collect())
-                .collect(),
-        }
-    }
-
-    fn count_color_in_row(row: &Vec<char>, color: char) -> usize {
-        row.iter()
-            .fold(0, |acc, char| acc + if *char == color { 1 } else { 0 })
+    fn parse(chars: &[char]) -> Self {
+        let pixels = chars.iter().cloned().collect();
+        Layer { pixels }
     }
 
     fn count_color(&self, color: char) -> usize {
         self.pixels
             .iter()
-            .fold(0, |acc, row| acc + Layer::count_color_in_row(row, color))
+            .fold(0, |acc, char| acc + if *char == color { 1 } else { 0 })
+    }
+
+    fn merge_layer_under(&self, other: &Layer) -> Layer {
+        let pixels: Vec<char> = self
+            .pixels
+            .iter()
+            .zip(other.pixels.iter())
+            .map(|(a, b)| if *a == '2' { *b } else { *a })
+            .collect();
+        Layer { pixels }
+    }
+
+    fn render(&self, width: usize) {
+        for row in self.pixels.chunks(width) {
+            dbg!(row.iter().collect::<String>());
+        }
     }
 }
 
 struct Image {
     layers: Vec<Layer>,
+    width: usize,
 }
 
 impl Image {
@@ -54,13 +61,22 @@ impl Image {
 
         let layers = pixels
             .chunks(width * height)
-            .map(|chunk| Layer::parse(chunk, width))
+            .map(|chunk| Layer::parse(chunk))
             .collect();
-        Image { layers }
+        Image { layers, width }
     }
 
     fn layer(&self, i: usize) -> &Layer {
         &self.layers[i]
+    }
+
+    fn show_merged(&self) {
+        let mut merged = self.layers[0].merge_layer_under(&self.layers[1]);
+        for i in 2..self.layers.len() {
+            merged = merged.merge_layer_under(&self.layers[i]);
+        }
+        // dbg!(merged);
+        merged.render(self.width);
     }
 }
 
@@ -76,8 +92,9 @@ fn main() {
         .expect("no height given")
         .parse::<usize>()
         .unwrap();
-    let mut img = Image::parse(&filename, width, height);
+    let img = Image::parse(&filename, width, height);
     let zeroes_layer_idx = img.find_layer_with_fewest_of_color('0');
     let zeroes_layer = img.layer(zeroes_layer_idx);
     dbg!(zeroes_layer.count_color('1') * zeroes_layer.count_color('2'));
+    img.show_merged();
 }
